f00ca209b2690e6a0ab2c6900bfc9831ffe5f67e
qel15 <aqel0205@gmail.com>
2024-11-20 15:12:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function navigateTo(theme) {
+    const pageName = theme.replace(/\s+/g, '-') + ".html";
+    window.location.href = Activities Page.html;
+}
+
+const form = document.getElementById('contactForm');
+const inputs = form.querySelectorAll('.item');
+const errorMessages = form.querySelectorAll('.error-txt');
+
+function validateField(field) {
+    const errorMessage = field.nextElement;
+
+    if (field.value.trim() === "") {
+        field.classList.add('invalid');
+        errorMessage.style.display = "block";
+    } else {
+        field.classList.remove('invalid');
+        errorMessage.style.display = "none";
+    }
+}
+
+
+function validateForm(event) {
+    let isValid = true;
+
+    inputs.forEach(input => {
+        validateField(input);
+
+        if (input.classList.contains('invalid')) {
+            isValid = false;
+        }
+    });
+
+    if (!isValid) {
+        event.preventDefault();
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('input', () => validateField(input));
+});
+
+form.addEventListener('submit', validateForm);
